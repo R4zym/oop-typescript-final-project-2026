bb864c311e753ad20e1de324011dae0cc602ac46
Yossaran Mankongdee <yossaran.p6.2.17.ccs@gmail.com>
2026-03-06 21:06:28 +0700

    update appModule & studentsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentModule } from './modules/student/students.module';
-import { CourseModule } from './modules/course/courses.module';
+import { CourseController } from './modules/course/courses.controller';
+import { CourseService } from './modules/course/courses.service';
 
 @Module({
-  imports: [StudentModule, CourseModule],
-  controllers: [CourseModule],
-  providers: [StudentModule],
+  imports: [StudentModule],
+  controllers: [CourseController],
+  providers: [CourseService],
 })
 export class AppModule {}
--- src/modules/student/students.module.ts
+++ src/modules/student/students.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './students.service';
 import { StudentController } from './students.controller';
+import { CourseModule } from '../course/courses.module';
 
 @Module({
-  imports: [],
+  imports: [CourseModule],
   controllers: [StudentController],
   providers: [StudentService],
   exports: [StudentService],
