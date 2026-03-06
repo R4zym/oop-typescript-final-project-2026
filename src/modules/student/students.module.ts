c71bd2bdd07b6eda0fa7203953bcbfa7ddc922ff
Renz-xz <woranon17340@gmail.com>
2026-03-06 14:28:14 +0700

    Update students.module.ts

---
+++ src/modules/student/students.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { StudentController } from './student.controller';
+
+@Module({
+  imports: [],
+  controllers: [StudentController],
+  providers: [StudentService],
+  exports: [StudentService],
+})
+export class StudentModule {}
